4078f108e77d4e407c21655fe77d3e5df3616341
julienco03 <julian160903@gmail.com>
2024-04-02 19:02:54 +0200

    Replace for with recursion and replace var with val

--- src/main/scala/de/htwg/se/Chess/model/BoardInterface.scala
+++ src/main/scala/de/htwg/se/Chess/model/BoardInterface.scala
@@ -3,7 +3,7 @@ package de.htwg.se.Chess.model
 import scala.collection.immutable.VectorMap
 
 trait BoardInterface(){
-    var board: VectorMap[String, String]
+    val board: VectorMap[String, String]
     def move(pos_now: String, pos_new: String): Board
     def game_finished(game_map: VectorMap[String, String]): Int
     def board_to_string(): String

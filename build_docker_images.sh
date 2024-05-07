3f6326b417bcd585b49430d5394011039c96141c
julienco03 <julian160903@gmail.com>
2024-05-07 12:10:44 +0200

    Docker is now working

--- build_docker_images.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-MODULES=("controller" "logic" "persistence" "rest" "ui" "utils")
-
-for module in "${MODULES[@]}"; do
-    docker build -t "$module"-image:latest $module
-done
-docker build -t src-image:latest .

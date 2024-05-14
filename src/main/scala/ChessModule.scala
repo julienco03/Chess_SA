9d0eb46e113fbe514904f356b5fa87ca591e5d52
julienco03 <julian160903@gmail.com>
2024-05-14 23:24:27 +0200

    Fix json logic

--- src/main/scala/ChessModule.scala
+++ src/main/scala/ChessModule.scala
@@ -4,7 +4,7 @@ import controller.ControllerInterface
 import controller.controllerComponent.Controller
 import model.Board
 import persistence._
-// import persistence.jsonImpl._
+import persistence.jsonImpl._
 import persistence.slickImpl._
 
 import com.google.inject.AbstractModule
@@ -13,8 +13,8 @@ import net.codingwell.scalaguice.ScalaModule
 
 class ChessModule extends AbstractModule {
     override def configure(): Unit = {
-        // bind[PersistenceInterface](new TypeLiteral[PersistenceInterface] {}).to(classOf[jsonImpl.FileIO])
-        // bind(classOf[ControllerInterface]).toInstance(new Controller(board = Board(), persistence = FileIO()))
+        // bind[PersistenceInterface](new TypeLiteral[PersistenceInterface] {}).to(classOf[jsonImpl.JsonFileIO])
+        // bind(classOf[ControllerInterface]).toInstance(new Controller(board = Board(), persistence = JsonFileIO()))
 
         bind[PersistenceInterface](new TypeLiteral[PersistenceInterface] {}).to(classOf[slickImpl.SlickDao])
         bind(classOf[ControllerInterface]).toInstance(new Controller(board = Board(), persistence = SlickDao()))

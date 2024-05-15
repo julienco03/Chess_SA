7ed43a058c6a56016b33b694a496bc9e2208f5b7
julienco03 <julian160903@gmail.com>
2024-05-15 13:09:41 +0200

    Files for fileIO are created automatically if they don't exist

--- src/main/scala/ChessModule.scala
+++ src/main/scala/ChessModule.scala
@@ -15,15 +15,15 @@ import net.codingwell.scalaguice.ScalaModule
 class ChessModule extends AbstractModule {
     override def configure(): Unit = {
         /* XML */
-        bind[PersistenceInterface](new TypeLiteral[PersistenceInterface] {}).to(classOf[xmlImpl.XmlFileIO])
-        bind(classOf[ControllerInterface]).toInstance(new Controller(board = Board(), persistence = XmlFileIO()))
+        // bind[PersistenceInterface](new TypeLiteral[PersistenceInterface] {}).to(classOf[xmlImpl.XmlFileIO])
+        // bind(classOf[ControllerInterface]).toInstance(new Controller(board = Board(), persistence = XmlFileIO()))
 
         /* JSON */
         // bind[PersistenceInterface](new TypeLiteral[PersistenceInterface] {}).to(classOf[jsonImpl.JsonFileIO])
         // bind(classOf[ControllerInterface]).toInstance(new Controller(board = Board(), persistence = JsonFileIO()))
 
         /* Slick - MySQL */
-        // bind[PersistenceInterface](new TypeLiteral[PersistenceInterface] {}).to(classOf[slickImpl.SlickDao])
-        // bind(classOf[ControllerInterface]).toInstance(new Controller(board = Board(), persistence = SlickDao()))
+        bind[PersistenceInterface](new TypeLiteral[PersistenceInterface] {}).to(classOf[slickImpl.SlickDao])
+        bind(classOf[ControllerInterface]).toInstance(new Controller(board = Board(), persistence = SlickDao()))
     }
 }
